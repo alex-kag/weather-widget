a2726514f541f6a0f005279ce99fb4edd555ed75
Alexei Kopendakov <kopendakov_a@itrail.by>
2023-06-20 12:02:23 +0300

    Добавлены 3 блока

---
+++ scripts/modules/render.js
@@ -0,0 +1,23 @@
+export const renderWidgetToday = (widget) =>{
+
+    widget.insertAdjacentHTML(
+        'beforeend',
+        '<div>Алекс1</div>'
+    )
+}
+
+export const renderWidgetOther = (widget) =>{
+
+    widget.insertAdjacentHTML(
+        'beforeend',
+        '<div>Алекс2</div>'
+    )
+}
+
+export const renderWidgetForecast = (widget) =>{
+
+    widget.insertAdjacentHTML(
+        'beforeend',
+        '<div>Алекс3</div>'
+    )
+}
