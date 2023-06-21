f29ff2addcec989ecacaf6d9890656c05acb199b
Alexei Kopendakov <kopendakov_a@itrail.by>
2023-06-21 11:18:05 +0300

    Переделано на сокращение кода

--- scripts/modules/util.js
+++ scripts/modules/util.js
@@ -1,3 +1,5 @@
+const addZerro = (n)=> n<10 ? `0${n}`:n;
+
 export const getCurrentDateTime = () => {
     // const months = [
     //     'Январь',
@@ -42,15 +44,8 @@ export const getCurrentDateTime = () => {
     const year = date.getFullYear();
     const dayOfWeek = weekdays[date.getDay()];
 
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    if (hours < 10){
-        hours = `0${hours}`;
-    }
-    if (minutes < 10){
-        minutes = `0${minutes}`;
-    }
+    const hours = addZerro(date.getHours());
+    const minutes = addZerro(date.getMinutes());
 
     return {dayOfMonth, month, year, dayOfWeek, hours, minutes};
-
 };
